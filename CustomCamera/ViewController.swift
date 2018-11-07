c1ad42b6a18c2c2bd47a47f74b1f9894dd162f69
Tito Albino Evangelista da Silva Junior <titoaesj@gmail.com>
2018-11-07 15:06:15 -0300

    Initial Commit

---
+++ CustomCamera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomCamera
+//
+//  Created by Tito Albino Evangelista da Silva Junior on 07/11/18.
+//  Copyright © 2018 br.com.titoaesj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
